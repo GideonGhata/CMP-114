327e40b6ff7961698fbff64986883a1a6946bb6f
GIDEON  GHATA <>
2023-04-17 13:46:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+todo = []
+list = ["cook","workout","ran","study"]
+todo.append("cook")
+todo.append("workout")
+todo.append("study")
+todo.append("laundry")
+for task in todo:
+    print(task)
+  
+
